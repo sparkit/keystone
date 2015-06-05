40e2e7edb8d25085daccec6029f72f6a30992ce4
Jimmy George Thomas <jimmygeorgethomas@gmail.com>
2015-06-05 12:21:19 +0530

    Revert "Primitive accesslevel support"
    This reverts commit 4b38e2f12cf2e20cc4a637efa04148bedacbe085.

--- admin/api/item/get.js
+++ admin/api/item/get.js
@@ -2,184 +2,161 @@ var _ = require('underscore');
 var async = require('async');
 var keystone = require('../../../');
 
-function skip(cb) {
-    cb();
-}
+function skip(cb) { cb(); }
 
-module.exports = function (req, res) {
+module.exports = function(req, res) {
 
-    var query = req.list.model.findById(req.params.id);
+	var query = req.list.model.findById(req.params.id);
 
-    if (req.list.tracking && req.list.tracking.createdBy) {
-        query.populate(req.list.tracking.createdBy);
-    }
+	if (req.list.tracking && req.list.tracking.createdBy) {
+		query.populate(req.list.tracking.createdBy);
+	}
 
-    if (req.list.tracking && req.list.tracking.updatedBy) {
-        query.populate(req.list.tracking.updatedBy);
-    }
+	if (req.list.tracking && req.list.tracking.updatedBy) {
+		query.populate(req.list.tracking.updatedBy);
+	}
 
-    query.exec(function (err, item) {
+	query.exec(function(err, item) {
 
-        if (err) return res.status(500).json({
-            err: 'database error',
-            detail: err
-        });
-        if (!item) return res.status(404).json({
-            err: 'not found',
-            id: req.params.id
-        });
+		if (err) return res.status(500).json({ err: 'database error', detail: err });
+		if (!item) return res.status(404).json({ err: 'not found', id: req.params.id });
 
-        if (!req.user.isAdmin
-            //if a standard user 
-            && (!item.createdBy
-                //tries to access items with no createdBy field,
-                //prevent it since no nonAdmin items lack the createdBy field
-                ||
-                //or tries to access items created by others,prevent it
-                item.createdBy._id.toString() !== req.user._id.toString())) {
-            console.log("unauthorised");
-            return;
-        }
+		var drilldown = {
+			def: req.list.get('drilldown'),
+			items: []
+		};
 
-        var drilldown = {
-            def: req.list.get('drilldown'),
-            items: []
-        };
+		var relationships;
 
-        var relationships;
+		/* Drilldown (optional, provided if ?drilldown=true in querystring) */
 
-        /* Drilldown (optional, provided if ?drilldown=true in querystring) */
+		var loadDrilldown = req.query.drilldown === 'true' && req.list.get('drilldown') ? function(cb) {
 
-        var loadDrilldown = req.query.drilldown === 'true' && req.list.get('drilldown') ? function (cb) {
+			// TODO: proper support for nested relationships in drilldown
+			
+			// step back through the drilldown list and load in reverse order to support nested relationships
+			drilldown.def = drilldown.def.split(' ').reverse();
 
-            // TODO: proper support for nested relationships in drilldown
+			async.eachSeries(drilldown.def, function(path, done) {
 
-            // step back through the drilldown list and load in reverse order to support nested relationships
-            drilldown.def = drilldown.def.split(' ').reverse();
+				var field = req.list.fields[path];
 
-            async.eachSeries(drilldown.def, function (path, done) {
+				if (!field || field.type !== 'relationship') {
+					throw new Error('Drilldown for ' + req.list.key + ' is invalid: field at path ' + path + ' is not a relationship.');
+				}
 
-                var field = req.list.fields[path];
+				var refList = field.refList;
 
-                if (!field || field.type !== 'relationship') {
-                    throw new Error('Drilldown for ' + req.list.key + ' is invalid: field at path ' + path + ' is not a relationship.');
-                }
+				if (field.many) {
+					if (!item.get(field.path).length) {
+						return done();
+					}
+					refList.model.find().where('_id').in(item.get(field.path)).limit(4).exec(function(err, results) {
+						if (err || !results) {
+							done(err);
+						}
+						var more = (results.length === 4) ? results.pop() : false;
+						if (results.length) {
+							// drilldown.data[path] = results;
+							drilldown.items.push({
+								list: refList.getOptions(),
+								items: _.map(results, function(i) {
+									return {
+										label: refList.getDocumentName(i),
+										href: '/keystone/' + refList.path + '/' + i.id
+									};
+								}),
+								more: (more) ? true : false
+							});
+						}
+						done();
+					});
+				} else {
+					if (!item.get(field.path)) {
+						return done();
+					}
+					refList.model.findById(item.get(field.path)).exec(function(err, result) {
+						if (result) {
+							// drilldown.data[path] = result;
+							drilldown.items.push({
+								list: refList.getOptions(),
+								items: [{
+									label: refList.getDocumentName(result),
+									href: '/keystone/' + refList.path + '/' + result.id
+								}]
+							});
+						}
+						done(err);
+					});
+				}
 
-                var refList = field.refList;
+			}, function(err) {
+				// put the drilldown list back in the right order
+				drilldown.def.reverse();
+				drilldown.items.reverse();
+				cb(err);
+			});
 
-                if (field.many) {
-                    if (!item.get(field.path).length) {
-                        return done();
-                    }
-                    refList.model.find().where('_id').in(item.get(field.path)).limit(4).exec(function (err, results) {
-                        if (err || !results) {
-                            done(err);
-                        }
-                        var more = (results.length === 4) ? results.pop() : false;
-                        if (results.length) {
-                            // drilldown.data[path] = results;
-                            drilldown.items.push({
-                                list: refList.getOptions(),
-                                items: _.map(results, function (i) {
-                                    return {
-                                        label: refList.getDocumentName(i),
-                                        href: '/keystone/' + refList.path + '/' + i.id
-                                    };
-                                }),
-                                more: (more) ? true : false
-                            });
-                        }
-                        done();
-                    });
-                } else {
-                    if (!item.get(field.path)) {
-                        return done();
-                    }
-                    refList.model.findById(item.get(field.path)).exec(function (err, result) {
-                        if (result) {
-                            // drilldown.data[path] = result;
-                            drilldown.items.push({
-                                list: refList.getOptions(),
-                                items: [{
-                                    label: refList.getDocumentName(result),
-                                    href: '/keystone/' + refList.path + '/' + result.id
-        }]
-                            });
-                        }
-                        done(err);
-                    });
-                }
+		} : skip;
 
-            }, function (err) {
-                // put the drilldown list back in the right order
-                drilldown.def.reverse();
-                drilldown.items.reverse();
-                cb(err);
-            });
+		/* Relationships (optional, provided if ?relationships=true in querystring) */
 
-        } : skip;
+		var loadRelationships = req.query.relationships === 'true' ? function(cb) {
 
-        /* Relationships (optional, provided if ?relationships=true in querystring) */
+			relationships = _.values(_.compact(_.map(req.list.relationships, function(i) {
+				if (i.isValid) {
+					return _.clone(i);
+				} else {
+					keystone.console.err('Relationship Configuration Error', 'Relationship: ' + i.path + ' on list: ' + req.list.key + ' links to an invalid list: ' + i.ref);
+					return null;
+				}
+			})));
 
-        var loadRelationships = req.query.relationships === 'true' ? function (cb) {
+			async.each(relationships, function(rel, done) {
 
-            relationships = _.values(_.compact(_.map(req.list.relationships, function (i) {
-                if (i.isValid) {
-                    return _.clone(i);
-                } else {
-                    keystone.console.err('Relationship Configuration Error', 'Relationship: ' + i.path + ' on list: ' + req.list.key + ' links to an invalid list: ' + i.ref);
-                    return null;
-                }
-            })));
+				// TODO: Handle invalid relationship config
+				rel.list = keystone.list(rel.ref);
+				rel.sortable = (rel.list.get('sortable') && rel.list.get('sortContext') === req.list.key + ':' + rel.path);
 
-            async.each(relationships, function (rel, done) {
+				// TODO: Handle relationships with more than 1 page of results
+				var q = rel.list.paginate({ page: 1, perPage: 100 })
+					.where(rel.refPath).equals(item.id)
+					.sort(rel.list.defaultSort);
 
-                // TODO: Handle invalid relationship config
-                rel.list = keystone.list(rel.ref);
-                rel.sortable = (rel.list.get('sortable') && rel.list.get('sortContext') === req.list.key + ':' + rel.path);
+				// rel.columns = _.reject(rel.list.defaultColumns, function(col) { return (col.type == 'relationship' && col.refList == req.list) });
+				rel.columns = rel.list.defaultColumns;
+				rel.list.selectColumns(q, rel.columns);
 
-                // TODO: Handle relationships with more than 1 page of results
-                var q = rel.list.paginate({
-                        page: 1,
-                        perPage: 100
-                    })
-                    .where(rel.refPath).equals(item.id)
-                    .sort(rel.list.defaultSort);
+				q.exec(function(err, results) {
+					rel.items = results;
+					done(err);
+				});
 
-                // rel.columns = _.reject(rel.list.defaultColumns, function(col) { return (col.type == 'relationship' && col.refList == req.list) });
-                rel.columns = rel.list.defaultColumns;
-                rel.list.selectColumns(q, rel.columns);
+			}, cb);
+			
+		} : skip;
 
-                q.exec(function (err, results) {
-                    rel.items = results;
-                    done(err);
-                });
+		/* Load data */
 
-            }, cb);
+		async.parallel([
+			loadDrilldown,
+			loadRelationships
+		], function(err) {
 
-        } : skip;
+			if (err) {
+				return res.status(500).json({
+					err: 'database error',
+					detail: err
+				});
+			}
 
-        /* Load data */
+			res.json({
+				data: req.list.getData(item),
+				drilldown: drilldown,
+				relationships: relationships
+			});
 
-        async.parallel([
-   loadDrilldown,
-   loadRelationships
-  ], function (err) {
+		});
 
-            if (err) {
-                return res.status(500).json({
-                    err: 'database error',
-                    detail: err
-                });
-            }
-
-            res.json({
-                data: req.list.getData(item),
-                drilldown: drilldown,
-                relationships: relationships
-            });
-
-        });
-
-    });
+	});
 };
